f8df202283da16f8d8a0822f2411f009ca3f8517
NewsGames <newssuporte@gmail.com>
2019-10-12 14:17:07 -0300

    Iniciando projeto TicketsLC MVC para estudos

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketsLC.Data;
+
+[assembly: HostingStartup(typeof(TicketsLC.Areas.Identity.IdentityHostingStartup))]
+namespace TicketsLC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
