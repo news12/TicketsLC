b7f3777688d2426037ee99b560a3d18a61331e9b
Italo Nunes Leite <italonleite@gmail.com>
2019-10-12 23:47:37 -0300

    - criando classe Tipo

---
+++ Models/Tipo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketsLC.Models
+{
+    public class Tipo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Tipo()
+        {
+
+        }
+
+        public Tipo(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
