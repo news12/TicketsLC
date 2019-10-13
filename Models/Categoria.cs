f169b38679649b0cb4d53c13cfd7e7cddb7cef21
Italo Nunes Leite <italonleite@gmail.com>
2019-10-13 00:32:42 -0300

    - criando construtor - criando relacionamento com a classe tipo

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -11,5 +11,20 @@ namespace TicketsLC.Models
         public int Nome { get; set; }
         public int Descricao { get; set; }
         public DateTime Data{ get; set; }
+        public Tipo IdTipo { get; set; }
+
+        public Categoria()
+        {
+
+        }
+
+        public Categoria(int id, int nome, int descricao, DateTime data, Tipo idTipo)
+        {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Data = data;
+            IdTipo = idTipo;
+        }
     }
 }
