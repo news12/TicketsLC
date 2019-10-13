a9c0bdc90a1f0f2f559096f98337a36e866d6868
NewsGames <newssuporte@gmail.com>
2019-10-13 01:05:16 -0300

    Criação de Migration Tabela Tickets, Implementação de Layout base nas viewes do Ticket

---
+++ Data/Migrations/20191013035457_AddTickets.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketsLC.Data.Migrations
+{
+    public partial class AddTickets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "Roles");
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdUsuario = table.Column<int>(nullable: false),
+                    IdCategoria = table.Column<int>(nullable: false),
+                    Titulo = table.Column<string>(maxLength: 50, nullable: false),
+                    Menssagem = table.Column<string>(maxLength: 500, nullable: false),
+                    IdDestinatario = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "Users",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "Roles",
+                column: "NormalizedName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "Roles");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "Users",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "Roles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+        }
+    }
+}
