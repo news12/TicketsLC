82dd271ec6c5ce25c7961f462844b6eb26e78da5
Italo Nunes Leite <italonleite@gmail.com>
2019-10-12 23:36:35 -0300

    - criando classe categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketsLC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public int Descricao { get; set; }
+        public DateTime Data{ get; set; }
+    }
+}
