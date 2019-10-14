e0328c2dacc4602db418eef039f79c77d777e9f7
Italo Nunes Leite <italonleite@gmail.com>
2019-10-13 22:30:55 -0300

    Criando mapeamento Tipo

---
+++ Data/Mapeamento/TipoMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketsLC.Models;
+
+namespace TicketsLC.Data
+{
+    public class TipoMap : IEntityTypeConfiguration<Tipo>
+    {
+        public void Configure(EntityTypeBuilder<Tipo> builder)
+        {
+            builder.ToTable("Tipos");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome);
+             
+        }
+    }
+
+}
